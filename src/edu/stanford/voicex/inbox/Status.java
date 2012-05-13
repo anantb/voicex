f53b53465131dfc2a16e7d823bba5b25fcf60b76
Anant Bhardwaj <anantb@anantb-lnx.(none)>
2012-05-13 13:56:52 -0700

    Status class

---
+++ src/edu/stanford/voicex/inbox/Status.java
@@ -0,0 +1,27 @@
+package edu.stanford.voicex.inbox;
+
+public class Status {
+	boolean ok;
+	String msg;
+	
+	public boolean isOk() {
+		return ok;
+	}
+	
+	public void setOk(boolean ok) {
+		this.ok = ok;
+	}
+	
+	public String getMsg() {
+		return msg;
+	}
+	
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
+	
+	
+	
+}
