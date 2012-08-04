3a1da4e890b899609d2dbf53c19ad9a0eee0dc48
Anant Bhardwaj <anantb@cs.stanford.edu>
2012-08-03 21:26:27 -0700

    moving all java code inside java folder

--- WebContent/inc/footer.jsp
+++
@@ -1,8 +0,0 @@
-<br />
-<a href="index.jsp">Home</a> | <a href="sms.jsp">SMS Demo</a>
-<hr />
-<small>
-&copy;Anant Bhardwaj, Designing Liberation Technology, Stanford University
-</small>
-</body>
-</html>
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-CLASSPATH=$CLASSPATH:./dist/voicex.jar:./lib/gson-2.2.1.jar:./lib/mysql-connector-java-5.1.20-bin.jar
-export CLASSPATH
-java -classpath $CLASSPATH edu.stanford.voicex.applications.Main
