065f0aae9a7a2b530baa77addf5e02b3da0ff709
Anant Bhardwaj <anantb@anantb-lnx.(none)>
2012-10-04 12:15:58 -0400

    remove all un-necessary abstractions

--- transport/voicex.py
+++ transport/voicex.py
@@ -20,7 +20,7 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-from abc import ABCMeta, abstractmethod
+
 from google_voice.main import GoogleVoice
 from africa_talking.main import AfricaTalking
 import config
@@ -34,39 +34,31 @@ VoiceX public APIs
 
 class VoiceX:
 	def __init__(self, client=config.GV):
-		__metaclass__ = ABCMeta
 		if(client == config.GV):
 			self.client = GoogleVoice(config.GV['username'], config.GV['password'], d = False)
 		elif(client == config.AT):
 			self.client = AfricaTalking(config.AP['username'], config.GV['api_key'])
 		else:
-			raise
-	
-	@abstractmethod	
+			raise NotImplementedError
+
 	def set_callback(self, callback):
 		self.client.set_callback(callback)
 
-	@abstractmethod
 	def sms(self, to, text):
 		self.client.sms(to, text)
 
-	@abstractmethod
 	def mark_read(self, msg):
 		self.client.mark_read(msg)
 
-	@abstractmethod
 	def mark_unread(self, msg):
 		self.client.mark_unread(msg)
 
-	@abstractmethod
 	def delete(self, msg):
 		self.client.delete(msg)
 
-	@abstractmethod
 	def fetch_unread_sms():
 		self.client.fetch_unread_sms()
 
-	@abstractmethod
 	def fetch_all_sms(self):
 		self.client.fetch_all_sms()
 
@@ -83,8 +75,6 @@ class Test():
 
 def main():	
 	Test()
-
-
 
 if __name__ == "__main__":
 	main()
