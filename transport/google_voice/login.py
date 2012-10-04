3af6acfd6c61bc4b8119e3fc5283ec121e028d6e
Anant Bhardwaj <anantb@anantb-lnx.(none)>
2012-10-04 01:26:48 -0400

    where is the transport

---
+++ transport/google_voice/login.py
@@ -0,0 +1,100 @@
+"""
+Copyright (c) 2012 Anant Bhardwaj
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import httplib, urllib, re, os
+from constants import *
+'''
+@author: anant bhardwaj
+@date: Aug 3, 2012
+
+voicex login
+'''
+accountType = "google";
+service = "grandcentral";
+source = "voicex";
+
+def login(email, password):
+	tokens = load_tokens()
+	if(tokens !=None):
+		t = tokens.split('|')
+		print "loaded tokens"
+		return {'auth':t[0], 'rnr_se':t[1]}
+		
+	else:
+		print "login reset"
+		tokens = login_reset(email, password)
+	return tokens
+	
+def load_tokens():
+	try:	
+		tokens = open('resources/' + 'voicex_token', 'rU').read()
+		return tokens
+	except IOError:
+		return None
+	
+
+def write_tokens(auth, rnr_se):
+	f = w_open('resources/' + 'voicex_token')
+	f.write('|'.join([auth, rnr_se]))
+	f.close()
+	
+def w_open(filename):
+	dir = os.path.dirname(filename)
+	try:
+		os.stat(dir)
+	except:
+		os.makedirs(dir)
+	return open(filename, 'w')
+
+def login_reset(email, password):
+	conn = httplib.HTTPSConnection("www.google.com")
+	headers = {"Content-type": "application/x-www-form-urlencoded",
+			"Accept": "text/plain"}
+	params = urllib.urlencode({'accountType': accountType, 
+						   'Email': email,
+						   'Passwd': password,
+						   'service': service,
+						   'source':source})	
+	conn.request("POST", LOGIN_URL, params, headers)
+	res = conn.getresponse().read()
+	print res
+	if('Auth=' in res):		
+		auth = res[res.find('Auth=')+len('Auth='):].strip()
+		print "auth success: " + auth
+	elif('Error=: ' in res):
+		error = res[res.find('Error=')+len('Error='):].strip()
+		print "auth failed: " + error
+	conn = httplib.HTTPSConnection("www.google.com")
+	conn.putrequest("GET", ROOT_URL)
+	conn.putheader( "Authorization", "GoogleLogin auth="+auth)
+	conn.endheaders()
+	res = conn.getresponse().read()
+	lines = re.split('\n', res)
+	for line in lines:
+		if("'_rnr_se': '" in line):
+			print line
+			rnr_se = line[line.find("'_rnr_se': '")+len("_rnr_se': "):-1]
+			rnr_se = rnr_se.replace("'",'').strip()
+			print rnr_se
+	write_tokens(auth, rnr_se)
+	return {'auth':auth, 'rnr_se':rnr_se}
---
+++ transport/google_voice/voicex.py
@@ -0,0 +1,124 @@
+"""
+Copyright (c) 2012 Anant Bhardwaj
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import json
+from constants import *
+from login import *
+from util import *
+from bs4 import BeautifulSoup
+from voicex_server import VoiceXServer
+
+'''
+@author: anant bhardwaj
+@date: Aug 3, 2012
+
+voicex public APIs
+'''	
+class VoiceX:
+	def __init__(self, email, password, d = True):
+		self.token = login(email, password)
+		self.d = d
+		
+	def start_server(self, callback):		
+		self.server = VoiceXServer(self, callback, d = self.d)
+		self.server.daemon = True
+		self.server.start()
+		self.server.join(1000)
+	
+	def sms(self, to_number, text):
+		print "Sending message [ %s ] to: [%s]." %(text, to_number)
+		params = {'phoneNumber': to_number, 'text':text, '_rnr_se':self.token['rnr_se']}
+		return http_post(SMS_SEND_URL, params, self.token['auth'])
+		
+
+	def call(self, forwardingNumber, outgoingNumber):
+		print "Initiating call to: "+outgoingNumber + ", through: " + forwardingNumber
+		params = {'forwardingNumber': forwardingNumber, 
+				  'outgoingNumber':outgoingNumber, 
+				  'phoneType':'1',
+				  'subscriberNumber':'undefined',
+				  'remember' : '0',
+				  '_rnr_se':token['rnr_se']}
+		return http_post(CALL_INITIATE_URL, params, self.token['auth'])
+		
+
+	def mark_read(self, msg):
+		print "Marking message [ %s ] as Read."  %(msg['messageText'])
+		params = {'messages': msg['id'], 'read':'1', '_rnr_se':self.token['rnr_se']}
+		return http_post(MSG_MARK_READ_URL, params, self.token['auth'])
+
+
+	def mark_unread(self, msg):
+		print "Marking message [ %s ] as UnRead."  %(msg['messageText'])
+		params = {'messages': msg['id'], 'read':'0', '_rnr_se':self.token['rnr_se']}
+		return http_post(MSG_MARK_READ_URL, params, self.token['auth'])
+
+		
+	def delete(self, msg):
+		print "Deleting message [ %s ]."  %(msg['messageText'])
+		params = {'messages': msg['id'], 'trash':'1', '_rnr_se':self.token['rnr_se']}
+		return http_post(MSG_DELETE_URL, params, self.token['auth'])	
+		
+
+	def fetch_unread_sms(self, url = SMS_UNREAD_URL):
+		return self.fetch_inbox(url = url)
+
+
+	def fetch_all_sms(self, url = SMS_URL):
+		return self.fetch_inbox(url = url)
+
+	def fetch_inbox(self, url = INBOX_URL):
+		conn = httplib.HTTPSConnection("www.google.com")
+		conn.putrequest("GET", url)
+		conn.putheader( "Authorization", "GoogleLogin auth="+self.token['auth'])
+		conn.endheaders()
+		page = conn.getresponse().read()
+		soup = BeautifulSoup(page)
+		meta_data = soup.find('json').find(text = True).strip()
+		print meta_data
+		return str(meta_data), page
+		
+
+
+class TestVoiceX():
+	def __init__(self, email, password):		
+		self.v = VoiceX(email, password, d = False)
+		self.v.start_server(callback = self.msg_new)
+		
+	def msg_new(self, msg):
+		print "Got text [ %s ] from [%s]." %(msg['messageText'], msg['phoneNumber'])
+		print self.v.mark_read(msg)
+		print self.v.sms(msg['phoneNumber'], "Ack :" + msg['messageText'])
+		print self.v.delete(msg)
+
+def main():	
+	TestVoiceX('voicex.git@gmail.com', 'VoiceX@Git')
+
+
+
+if __name__ == "__main__":
+	main()
+	
+	
+					
+			
