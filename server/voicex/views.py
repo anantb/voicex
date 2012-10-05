15da46083c7a5d0fc3f71a21974b550c318528c4
Anant Bhardwaj <anantb@voicex.csail.mit.edu>
2012-10-04 23:58:51 -0400

    more changes

--- server/voicex/views.py
+++ server/voicex/views.py
@@ -2,6 +2,7 @@ from django.http import *
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import *
 import json
+from apps.trish.main import Trish
 
 def index(request):
 	return render_to_response("index.html")
@@ -10,6 +11,8 @@ def index(request):
 @csrf_exempt
 def trish(request):
 	if(request.POST):
+		t = Trish()
+		t.msg_new(request.POST)
 		res = {'status':True, 'request':request.POST, 'msg':'response sent to your number'}
 		return HttpResponse(json.dumps(res))
 	else:
--- server/voicex/wsgi.py
+++ server/voicex/wsgi.py
@@ -13,8 +13,8 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
-
+import os, sys
+sys.path.append('/workspace/voicex')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voicex.settings")
 
 # This application object is used by any WSGI server configured to use this
