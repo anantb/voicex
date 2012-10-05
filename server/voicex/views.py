edc51511e884ba9d4dad2ba6012f051b77176e0e
Anant Bhardwaj <anantb@anantb-lnx.(none)>
2012-10-05 01:15:10 -0400

    handle GV HTTP POST

--- server/voicex/views.py
+++ server/voicex/views.py
@@ -11,8 +11,15 @@ def index(request):
 @csrf_exempt
 def trish(request):
 	if(request.POST):
+		msg_data = {}
+		if('number' in request.POST):
+			msg_data['from'] = request.POST['number']
+		if('text' in request.POST):
+			msg_data['text'] = request.POST['text']
+		if('from' in request.POST):
+			msg_data['from'] = request.POST['from']
 		t = Trish()
-		t.msg_new(request.POST)
+		t.msg_new(msg_data)
 		res = {'status':True, 'request':request.POST, 'msg':'response sent to your number'}
 		return HttpResponse(json.dumps(res))
 	else:
