238e9c14735384c7efd7a210e5a0202bef9f4101
Anant Bhardwaj <anantb@anantb-lnx.(none)>
2012-10-05 16:20:56 -0400

    matching order

--- server/voicex/urls.py
+++ server/voicex/urls.py
@@ -8,10 +8,10 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$','voicex.views.index'),
     url(r'^index', 'voicex.views.index'),
-    url(r'^voicex', 'voicex.views.voicex_us'),
-    url(r'^mungano', 'voicex.views.mungano_us'),
     url(r'^voicex/us', 'voicex.views.voicex_us'),
     url(r'^mungano/us', 'voicex.views.mungano_us'),
     url(r'^voicex/ke', 'voicex.views.voicex_ke'),
-    url(r'^mungano/ke', 'voicex.views.mungano_ke')
+    url(r'^mungano/ke', 'voicex.views.mungano_ke'),
+    url(r'^voicex$', 'voicex.views.voicex_us'),
+    url(r'^mungano$', 'voicex.views.mungano_us')
 )
