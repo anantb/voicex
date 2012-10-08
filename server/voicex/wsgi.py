f2c4162c32bb214b0b54058f138632c7155d471d
Anant Bhardwaj <anantb@voicex.csail.mit.edu>
2012-10-08 01:22:25 -0400

    server configuration

--- server/voicex/wsgi.py
+++ server/voicex/wsgi.py
@@ -14,7 +14,16 @@ framework.
 
 """
 import os, sys
-sys.path.append('/workspace/voicex')
+
+p = os.path.abspath(os.path.dirname(__file__))
+if(os.path.abspath(p+"/../..") not in sys.path):
+	print os.path.abspath(p+"/../..")
+	sys.path.append(os.path.abspath(p+"/../.."))
+
+if(os.path.abspath(p+"/..") not in sys.path):
+	print os.path.abspath(p+"/..")
+	sys.path.append(os.path.abspath(p+"/.."))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voicex.settings")
 
 # This application object is used by any WSGI server configured to use this
