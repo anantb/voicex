7ab1712b43760f3547d1a68686bf1afe39357b51
Anant Bhardwaj <anantb@csail.mit.edu>
2012-12-03 01:29:09 -0500

    for future

---
+++ voicex/msg_codes.py
@@ -0,0 +1,8 @@
+msg_code={
+	'NAME_NOT_AVAILABLE': 'Name not available',
+	'ACCOUNT_NAME_NOT_FOUND_ERROR': 'Account not found',
+	'POST_NOT_FOUND_ERROR': 'Post not found',
+	'NUMBER_REGISTERED': 'Number Registered with other account name',
+	'REQUEST_ERROR': 'Incorrect request parameters',
+	'UNKNOWN_ERROR': 'Unknown Error',
+}
