a5c94cdb74112812a71f140e3129c0f0eba74429
Anant Bhardwaj <anantb@csail.mit.edu>
2013-01-19 19:44:39 -0500

    logging configuration -- logfile in the root

--- http_handler/settings.py
+++ http_handler/settings.py
@@ -152,7 +152,7 @@ LOGGING = {
         'logfile': {
             'level':'DEBUG',
             'class':'logging.handlers.RotatingFileHandler',
-            'filename': SITE_ROOT + "/logfile",
+            'filename':"voicex.log",
             'maxBytes': 50000,
             'backupCount': 2,
             'formatter': 'standard',
