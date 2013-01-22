1558545fec2253d4ab9cd494543051dc4692d301
Anant Bhardwaj <anantb@csail.mit.edu>
2013-01-21 21:16:17 -0500

    a boilerplate for http handler with express

--- http_handler/main.js
+++ http_handler/main.js
@@ -1,8 +1,8 @@
+var express = require('express');
+var app = express();
 
-var http = require('http');
-http.createServer(function (req, res) {
-  console.log(req);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
+app.get('/', function(req, res){
+  res.send('hello voicex');
+});
+
+app.listen(3000);
